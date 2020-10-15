f5b1d5d403dc116655d9606cfb7963663ca66db8
helenohyeah <ouyang.helen@gmail.com>
2020-10-15 22:55:45 +0000

    Remove debugging code

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -135,7 +135,6 @@ const getAllProperties = function(options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-  console.log(queryString, queryParams);
   return pool.query(queryString, queryParams)
     .then(res => res.rows);
 };
@@ -147,11 +146,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
-  console.log(property);
   return pool.query(`
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, street, province, post_code)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
